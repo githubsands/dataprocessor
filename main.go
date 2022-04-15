e15fa617f9a7485a033acf8eee6a91b26796a821
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2022-04-14 18:01:17 -0600

    Refactors to proper main function

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`") // TODO: Add this back in
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
-	processor()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	processor(ctx, getConfig())
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case sig := <-signals:
+		cancel()
+		time.Sleep(5 * time.Second)
+		fmt.Println(sig.String())
+		os.Exit(1)
+	default:
+	}
 }
 
-func processor() {
-	ctx := context.Background()
-	cfg := getConfig()
+func processor(ctx context.Context, cfg *config) error {
 	p := NewProcessor(ctx, cfg.Samples, cfg.BatchDuration, os.Stdin)
 	p.ProcessLogs(ctx)
+	return nil
 }
